3cf8ebccb3ff18c48bda6d83ddd425fdb3930310
RIJUL S <rijul.s2024@vitstudent.ac.in>
2026-05-30 11:12:00 +0530

    feat: implement BLE service enumeration

--- ble_client.py
+++ ble_client.py
@@ -1,10 +1,16 @@
 import asyncio
 # pyrefly: ignore [missing-import]
-from bleak import BleakScanner
+from bleak import BleakClient
+ADDRESS="44:27:F3:21:47:BC"
 
 async def main():
-    devices = await BleakScanner.discover()
-    for d in devices:
-        print(f'{d.name}\n {d.address}\n')
+    async with BleakClient(ADDRESS) as client:
+        print(f"Connected to {client.address}")
+        for service in client.services:
+            print(f"service:{service.uuid}")
+            for characteristic in service.characteristics:
+                print(f"char:{characteristic.uuid}")
+                print(f"charproperties:{characteristic.properties}")
+                print()
 if __name__=="__main__":
     asyncio.run(main()) 
